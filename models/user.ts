fef6521fdbad35d9fa93d9ff1d4b9063501c3a9c
yafet-123 <yafetaddisu123@gmail.com>
2023-05-18 15:10:12 -0700

    adding the monoose model

---
+++ models/user.ts
@@ -0,0 +1,22 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, 'Email already exists!'],
+    required: [true, 'Email is required!'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required!'],
+    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+  },
+  image: {
+    type: String,
+  }
+});
+
+const User = models.User || model("User", UserSchema); // to prevent creating the model again  check the model user if it is not create if it there use the previous one
+// because every time the connection established the connection create
+
+export default User;
