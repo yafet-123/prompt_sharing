b8f77ca0af034f0d254d47a94214e3d648ac4a7a
yafet-123 <yafetaddisu123@gmail.com>
2023-05-19 03:03:23 -0700

    creating the update-prompt

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,6 +1,6 @@
-import Prompt from "@models/prompt";
+import Prompt from "@/models/prompt";
 import { connectToDB } from "@utils/database";
-
+ 
 export const GET = async (request, { params }) => {
     try {
         await connectToDB()
