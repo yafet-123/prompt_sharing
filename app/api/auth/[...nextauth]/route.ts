0ba0243e392c12734ac94cd21bc509caec530d1f
yafet-123 <yafetaddisu123@gmail.com>
2023-05-21 02:08:30 -0700

    fixing the error in api auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,7 +9,7 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
+    }),
   ],
   callbacks: {
     async session({ session }) {
